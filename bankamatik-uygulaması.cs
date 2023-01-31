8db2da288861e77a1eeb77807d27c577b684ba41
Ahmet Metin Arslan <94191552+ahmetmetinarslan@users.noreply.github.com>
2023-01-31 20:58:14 +0300

    Add files via upload

---
+++ bankamatik-uygulaması.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] arg) {
+             
+        // Bankamatik
+
+        // Bakiye, Para Yatırma, Para Çekme, Çıkış.
+
+        string secim="";
+        double bakiye=0;
+        double ekhesap = 1000;
+        double ekhesapLimiti = 1000;
+
+        do
+        {
+            Console.WriteLine("1-Bakiye Görüntüle\n2-Para Yatırma\n3-Para Çek\n4-Güvenli Çıkış\nSeçiminiz: ");
+            secim = Console.ReadLine();
+            switch (secim)
+            {
+                case "1":
+                    Console.WriteLine("Bakiyeniz {0} TL", bakiye);
+                    Console.WriteLine("Ek Hesap Bakiyeniz {0} TL", ekhesap);
+                    break;
+                case "2":
+                    Console.Write("Yatırmak İstediğiniz Miktar: ");
+                    double yatirilan = double.Parse(Console.ReadLine());
+
+                    if (ekhesap<ekhesapLimiti)
+                    {
+                        double ekhesaptankullanilan = ekhesapLimiti - ekhesap;
+                        if (yatirilan>=ekhesaptankullanilan)
+                        {
+                            ekhesap = ekhesapLimiti;
+                            bakiye = yatirilan - ekhesaptankullanilan;
+                        }
+                        else
+                        {
+                          ekhesap += yatirilan; 
+                        }
+                    }
+                    else
+                    {
+                        bakiye+=yatirilan;
+                    }
+                    break;
+                case "3":
+                    Console.Write("Çekmek İstediğniz Miktar: ");
+                    double cekilenmiktar = double.Parse(Console.ReadLine());
+                    if (cekilenmiktar>bakiye)
+                    {
+                        double toplam = bakiye + ekhesap;
+                        if (toplam>=cekilenmiktar)
+                        {
+                            Console.WriteLine("Ek Hesap Kullanılsın mı?(e/h)");
+                            string ekhesaptercihi = Console.ReadLine();
+
+                            if (ekhesaptercihi=="e")
+                            {
+                                Console.Write("Paranızı Alabilirsiniz.");
+                                ekhesap -= (cekilenmiktar-bakiye);
+                                bakiye = 0;
+                            }
+                            else
+                            {
+                                Console.Write("Bakiyeniz Yetersiz.");
+                            }    
+                        }
+                    }
+                    else   
+                    {
+                        Console.Write("Paranızı Alabilirsiniz.");
+                        bakiye-=cekilenmiktar;
+                    } 
+                        
+                    break;
+                case "4":
+                    Console.Write("Çıkış");
+                    break;
+                default:
+                    Console.Write("Hatalı Seçim");
+                    break;
+            }
+        } while (secim!="4");
+        Console.WriteLine("Güvenli Çıkış Yapıldı.");
+        }
+    }
+}
